9da67d318e957ba8158838aaa710099e53174a27
spcrey <spcrey@outlook.com>
2024-10-02 19:51:08 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, Cpp!" << endl;
+    return 0;
+}
---
+++ run.sh
@@ -0,0 +1,29 @@
+# Python
+python main.py
+
+# C
+gcc main.c -o main && ./main && rm ./main
+
+# Cpp
+g++ main.cpp -o main && ./main && rm ./main
+
+# Html
+python -m http.server
+
+# Java Script
+node script.js
+
+# Java
+javac Main.java && java Main && rm ./Main.class
+
+# kotlin
+kotlinc main.kt && java MainKt && rm ./MainKt.class
+
+# Sqlite
+sqlite3 database.db
+
+# Go
+go run main.go
+
+# Type Script
+tsc main.ts && node main.js && rm main.js
