8bdd6b43752c9c6fd20090a422a1816ad4132be3
spcrey <spcrey@outlook.com>
2024-10-09 21:14:43 +0800

    add jni

--- jni/run.sh
+++ jni/run.sh
@@ -2,7 +2,6 @@ JAVA_HOME="/usr/lib/jvm/java-1.17.0-openjdk-amd64"
 LIBRARY_PATH="/home/crey/learn-programming-language/jni"
 
 javac -h . HelloNative.java
-gcc -m64 -fPIC -shared -o libHelloNative.so\
- -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" HelloNative.c
+gcc -m64 -fPIC -shared -o libHelloNative.so -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" HelloNative.c
 java -Djava.library.path=$LIBRARY_PATH:$JAVA_LIBRARY_PATH HelloNative
 
